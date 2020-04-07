f903c50ecac4e00e72ebc942f904121525b2c8c5
Dieter Warson <dieter.warson@student.uhasselt.be>
2020-04-07 17:02:31 +0200

    Fix for windows

--- ReportingSystem/startServerDev.sh
+++ ReportingSystem/startServerDev.sh
@@ -1,9 +1,9 @@
-cd psopv-2020-groep1/ReportingSystem
-
 /etc/init.d/mysql start
 
 mysql -fu root -pmysqlroot < "mysql_secure_install.sql"
 mysql -fu root -pmysqlroot < "testdata.sql"
+
+cd psopv-2020-groep1/ReportingSystem
 
 cd reportingsystemVue
 npm install 
