3466087ccb99749c426d9a2e4070e01ce27fe07d
Dieter Warson <dieter.warson@student.uhasselt.be>
2020-04-07 13:24:44 +0200

    Database working with SQL import

--- ReportingSystem/startServerDev.sh
+++ ReportingSystem/startServerDev.sh
@@ -1,10 +1,16 @@
 
+/etc/init.d/mysql start
+
+mysql -fu root -pmysqlroot < "mysql_secure_install.sql"
+mysql -fu root -pmysqlroot < "testdata.sql"
+
 cd psopv-2020-groep1/ReportingSystem/reportingsystemVue 
 npm install 
 yarn build --dest ../reportingsystemExpress/src/public 
 cd -
 cd psopv-2020-groep1/ReportingSystem/reportingsystemExpress 
 npm install
+
 
 cd reportingsystemExpress
 npm run start:dev
