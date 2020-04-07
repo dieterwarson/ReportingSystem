80d8d5cfbe43b465b40f437ba597b94ba02c7116
Dieter Warson <dieter.warson@student.uhasselt.be>
2020-04-07 16:42:42 +0200

    DockerDev fix

--- ReportingSystem/startServerDev.sh
+++ ReportingSystem/startServerDev.sh
@@ -1,16 +1,15 @@
+cd psopv-2020-groep1/ReportingSystem
 
 /etc/init.d/mysql start
 
 mysql -fu root -pmysqlroot < "mysql_secure_install.sql"
 mysql -fu root -pmysqlroot < "testdata.sql"
 
-cd psopv-2020-groep1/ReportingSystem/reportingsystemVue 
+cd reportingsystemVue
 npm install 
 yarn build --dest ../reportingsystemExpress/src/public 
 cd -
-cd psopv-2020-groep1/ReportingSystem/reportingsystemExpress 
+cd reportingsystemExpress 
 npm install
 
-
-cd reportingsystemExpress
 npm run start:dev
