4d8c9ddd062fc8a148b842b9691b4e030a1953b0
Dieter Warson <dieter.warson@student.uhasselt.be>
2020-04-11 16:14:50 +0200

    added database object

--- ReportingSystem/reportingsystemExpress/src/models/index.ts
+++ ReportingSystem/reportingsystemExpress/src/models/index.ts
@@ -1,13 +1,24 @@
 import { Sequelize } from "sequelize";
 import { setupConnection } from "../config/config";
+import { Report } from "./report"
 
-let config = new setupConnection();
 
-let sequelize = new Sequelize(config.getDatabase());
+export class Database{
+  config: setupConnection = new setupConnection();
+  sequelize: Sequelize = new Sequelize(this.config.getDatabase());
+  report: Report = new Report
 
-try {
-  sequelize.authenticate();
-  console.log("Connection has been established successfully.");
-} catch (error) {
-  console.error("Unable to connect to the database:", error);
+  constructor() {
+    this.sequelize.sync()
+  }
+
+  testConnection(){
+    try {
+      this.sequelize.authenticate();
+      console.log("Connection has been established successfully.");
+    } catch (error) {
+      console.error("Unable to connect to the database:", error);
+    }
+  }
+
 }
