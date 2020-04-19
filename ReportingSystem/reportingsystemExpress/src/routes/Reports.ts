db23b676245260fe3ad2527aa643b6e349d8149c
MathiasJans <mathias_jans@student.uhasselt.be>
2020-04-19 14:56:01 +0200

    page gets response from router

--- ReportingSystem/reportingsystemExpress/src/routes/Reports.ts
+++ ReportingSystem/reportingsystemExpress/src/routes/Reports.ts
@@ -1,18 +1,28 @@
 import { Request, Response, Router } from "express";
 import { OK } from "http-status-codes";
-import Sequelize from "../Server"
+import Sequelize from "../Server";
 
 // Init router
 const router = Router();
-
 
 /******************************************************************************
  *                      Get All Reports - "GET /api/reports/recieve"
  ******************************************************************************/
 
 router.get("/recieve", async (req: Request, res: Response) => {
-  const reports = Sequelize.get('search');  
-  return res.status(OK).json({ reports });
+  const reports = Sequelize.get("SecretariatNotifications");
+  // set() werkt al een dictionary
+  // app.set('foo', 'bar');
+  // get() geeft de waarde terug die was ingegeven bij de set
+  // app.get('foo');
+  // => "bar"
+  const length = Sequelize.length;
+  console.log('Sequelize.length: ' + length);
+  
+  return res.status(OK).json({
+    reports,
+    length, 
+  });
 });
 
 /******************************************************************************
