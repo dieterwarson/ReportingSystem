56841b930f00568e940486f8b7a5b65079a29f4b
MathiasJans <mathias_jans@student.uhasselt.be>
2020-04-19 22:17:57 +0200

    added working gets

--- ReportingSystem/reportingsystemExpress/src/routes/Reports.ts
+++ ReportingSystem/reportingsystemExpress/src/routes/Reports.ts
@@ -1,18 +1,47 @@
 import { Request, Response, Router } from "express";
 import SecretariatNotifications from "../models/secretariatNotification";
+import OperationalEvent from "src/models/operationalEvent";
+import SecretariatNotification from "../models/secretariatNotification";
+import { Op } from "sequelize";
 
 // Init router
 const router = Router();
-
 
 /******************************************************************************
  *                      Get All Reports - "GET /api/reports/recieve"
  ******************************************************************************/
 
-router.get("/recieve", (req: Request, res: Response) => {
-  SecretariatNotifications.findAll().then((secretariatNotifications: any) =>
-    res.send(secretariatNotifications)
-  );
+router.get("/recieve/", async (req: Request, res: Response) => {
+  const search = "Japn";
+
+  var result;
+  result = await SecretariatNotification.findOne({
+    where: {
+      description: {
+        [Op.like]: "%" + search + "%",
+        // Hier moet nog een OR of een AND of iets bij zodat er op meerdere columns in 1 table gecheckt kan worden
+      },
+    },
+  });
+  if (result !== null) {
+    res.send(result);
+  }
+
+  result = await OperationalEvent.findOne({
+    where: {
+      description: {
+        [Op.like]: "%" + search + "%",
+        // Hier moet nog een OR of een AND of iets bij zodat er op meerdere columns in 1 table gecheckt kan worden
+      },
+    },
+  });
+  if (result !== null) {
+    res.send(result);
+  }
+
+  // Er moet nog een findOne() komen voor
+  // Defect, Malfunction, Replacement, WorkplaceEvent
+
 });
 
 /******************************************************************************
@@ -20,3 +49,4 @@ router.get("/recieve", (req: Request, res: Response) => {
  ******************************************************************************/
 
 export default router;
+mmm;
