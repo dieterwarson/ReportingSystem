e3ae1b93b600726b8fc8f79f7be157360bee7a7c
Dieter Warson <dieter.warson@student.uhasselt.be>
2020-04-23 14:18:38 +0200

    express route for getting the details of a given report

--- ReportingSystem/reportingsystemExpress/src/routes/Reports.ts
+++ ReportingSystem/reportingsystemExpress/src/routes/Reports.ts
@@ -10,13 +10,14 @@ import Malfunction from 'src/models/malfunction';
 import Replacement from 'src/models/replacement';
 import WorkplaceEvent from 'src/models/workplaceEvent';
 import { Sequelize } from 'sequelize-typescript';
-import { sequelize } from 'src/config/config';
+import Administrative from 'src/models/administrative';
+import Technical from 'src/models/technical';
 
 // Init router
 const router = Router();
 
 /******************************************************************************
- *                      Get All Reports - "GET /api/reports/all"
+ *                   Get All Reports - "GET /api/reports/all"
  ******************************************************************************/
 
 // only get the reports that are finished
@@ -34,7 +35,7 @@ router.get('/all', async (req: Request, res: Response) => {
 });
 
 /******************************************************************************
- *                      Get All monitored Reports - "GET /api/reports/monitored"
+ *             Get All monitored Reports - "GET /api/reports/monitored"
  ******************************************************************************/
 
 // only get the reports that are finished and arer being monitored
@@ -188,6 +189,42 @@ router.get('/search/', async (req: Request, res: Response) => {
     return res.json({ result });
   }
 });
+
+
+/******************************************************************************
+ *        Get the content of a report - "GET /api/reports/content/:reportId"
+ ******************************************************************************/
+router.get('/content/:reportId', async (req: Request, res: Response) => {
+  let reportId = req.param('reportId');
+  let report = await Report.findOne({
+    where: {
+      id: reportId,
+    }
+  });
+
+  let technical = await report?.$get('technical');
+  let administrative = await report?.$get('administrative');
+  let operational = await report?.$get('operational');
+
+  let defects = await technical?.$get('defects');
+  let malfunctions = await technical?.$get('malfunctions');
+  let replacements = await administrative?.$get('replacements');
+  let workplaceEvents = await administrative?.$get('workplaceEvents');
+  let secretariatNotifications = await administrative?.$get('replacements');
+  let operationalEvents = await operational?.$get('operationalEvents');
+
+  let results = {'operational': {operationalEvents},
+                 'administrative': {replacements, workplaceEvents, secretariatNotifications},
+                 'technical': {defects, malfunctions},
+                 };
+  
+  res.send(results);
+
+
+  // res.send(report);
+  // return res.json({ report });
+});
+
 
 /******************************************************************************
  *                                     Export
