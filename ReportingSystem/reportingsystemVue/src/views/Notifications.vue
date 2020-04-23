02159e7cd3aff601c517fe63289f7548a05ab370
Dieter Warson <dieter.warson@student.uhasselt.be>
2020-04-23 22:22:37 +0200

    fix notifications

--- ReportingSystem/reportingsystemVue/src/views/Notifications.vue
+++ ReportingSystem/reportingsystemVue/src/views/Notifications.vue
@@ -1,63 +1,12 @@
 <template>
   <div class="container pt-5 pb-5">
     <h1>
-      Meldignen
+      Meldingen
       {{ new Date(reportContent.report.date).toLocaleString('en-BE') }}
     </h1>
-    <div
-      class="btn-group d-flex"
-      role="group"
-      aria-label="Justified button group"
-    >
-      <button
-        type="button"
-        class="btn btn-primary"
-        @click.prevent="getOperational"
-      >
-        Operationeel
-      </button>
-      <button
-        type="button"
-        class="btn btn-primary"
-        @click.prevent="getWorkforce"
-      >
-        Personeel
-      </button>
-      <button
-        type="button"
-        class="btn btn-primary"
-        @click.prevent="getTechnical"
-      >
-        Technisch
-      </button>
-    </div>
 
-    <section v-if="step == 'Operational'">
-      <div v-if="this.reportContent.operational == {}">
-        <p>Er zijn nog geen gebeurtenissen van deze categorie</p>
-      </div>
-      <div v-else>
-        <div
-          v-for="event in reportContent.operational.operationalEvents"
-          :key="event.id"
-        >
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">
-                {{ new Date(event.date).toLocaleString('en-BE') }}
-              </h5>
-              <p class="card-text">{{ event.signaling }}</p>
-              <p class="card-text">{{ event.plNumber }}</p>
-              <p class="card-text">{{ event.description }}</p>
-              <p class="card-text">{{ event.location }}</p>
-              <p class="card-text">{{ event.unit }}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <section v-if="step == 'Workforce'">
+    <section>
+      <h2>Administratief</h2>
       <div v-if="this.reportContent.administrative == {}">
         <p>Er zijn nog geen gebeurtenissen van deze categorie</p>
       </div>
@@ -114,7 +63,8 @@
       </div>
     </section>
 
-    <section v-if="step == 'Technical'">
+    <section>
+      <h2>Technisch</h2>
       <div v-if="this.reportContent.technical == {}">
         <p>Er zijn nog geen gebeurtenissen van deze categorie</p>
       </div>
@@ -172,29 +122,7 @@ export default Vue.extend({
       //   '/api/reports/monitored' + this.$route.query.reportId
       // ).then((res) => (this.reportContent = res));
 
-      this.reportContent = {
-        report: {
-          id: 1,
-          date: '2020-03-16T21:13:48.000Z',
-          temporary: false,
-          createdAt: '2020-04-23T14:25:02.000Z',
-          updatedAt: '2020-04-23T14:25:02.000Z',
-        },
-        operational: {},
-        administrative: {
-          id: 1,
-          administrativeId: 1,
-          authorId: 1,
-          absentee: 'Jan Jacobs',
-          substitute: 'Geordy Hendricks',
-          monitoring: true,
-          date: '2020-03-30T15:46:36.000Z',
-          shift: true,
-          createdAt: '2020-04-23T18:43:57.000Z',
-          updatedAt: '2020-04-23T18:43:57.000Z',
-        },
-        technical: {},
-      };
+      this.reportContent = {"report":{"id":1,"date":"2020-03-16T21:13:48.000Z","temporary":false,"createdAt":"2020-04-23T20:00:09.000Z","updatedAt":"2020-04-23T20:00:09.000Z"},"operational":{"operationalEvents":[{"id":1,"operationalId":1,"authorId":1,"operationalTypeId":1,"monitoring":true,"signaling":"Verlies inschrijvingsbewijs","plNumber":null,"description":null,"location":null,"unit":"KEMPLA","date":"2020-03-16T18:13:48.000Z","createdAt":"2020-04-23T20:00:09.000Z","updatedAt":"2020-04-23T20:00:09.000Z"},{"id":2,"operationalId":1,"authorId":1,"operationalTypeId":2,"monitoring":true,"signaling":null,"plNumber":"PL031770168","description":null,"location":null,"unit":"CARMA","date":"2020-03-16T21:34:37.000Z","createdAt":"2020-04-23T20:00:09.000Z","updatedAt":"2020-04-23T20:00:09.000Z"},{"id":3,"operationalId":1,"authorId":1,"operationalTypeId":3,"monitoring":true,"signaling":null,"plNumber":"PL03170104","description":null,"location":null,"unit":"HANO","date":"2020-03-16T22:05:18.000Z","createdAt":"2020-04-23T20:00:09.000Z","updatedAt":"2020-04-23T20:00:09.000Z"},{"id":4,"operationalId":1,"authorId":1,"operationalTypeId":4,"monitoring":true,"signaling":"Seining persoon","plNumber":null,"description":null,"location":null,"unit":"LAMA","date":"2020-03-16T23:34:33.000Z","createdAt":"2020-04-23T20:00:09.000Z","updatedAt":"2020-04-23T20:00:09.000Z"},{"id":5,"operationalId":1,"authorId":1,"operationalTypeId":5,"monitoring":true,"signaling":"Seining persoon","plNumber":null,"description":null,"location":null,"unit":"LOON","date":"2020-03-16T23:57:10.000Z","createdAt":"2020-04-23T20:00:09.000Z","updatedAt":"2020-04-23T20:00:09.000Z"},{"id":6,"operationalId":1,"authorId":1,"operationalTypeId":6,"monitoring":true,"signaling":"Seining persoon","plNumber":"PL03170202","description":null,"location":null,"unit":"BIHORI","date":"2020-03-16T00:18:57.000Z","createdAt":"2020-04-23T20:00:09.000Z","updatedAt":"2020-04-23T20:00:09.000Z"},{"id":7,"operationalId":1,"authorId":1,"operationalTypeId":7,"monitoring":true,"signaling":null,"plNumber":"PL03170104","description":null,"location":null,"unit":"HANO","date":"2020-03-16T00:45:45.000Z","createdAt":"2020-04-23T20:00:09.000Z","updatedAt":"2020-04-23T20:00:09.000Z"},{"id":8,"operationalId":1,"authorId":1,"operationalTypeId":8,"monitoring":true,"signaling":null,"plNumber":"PL03170315","description":null,"location":null,"unit":"LRH","date":"2020-03-16T01:21:25.000Z","createdAt":"2020-04-23T20:00:09.000Z","updatedAt":"2020-04-23T20:00:09.000Z"},{"id":9,"operationalId":1,"authorId":1,"operationalTypeId":9,"monitoring":true,"signaling":null,"plNumber":"PL03170322","description":null,"location":null,"unit":"LRH","date":"2020-03-16T01:51:47.000Z","createdAt":"2020-04-23T20:00:09.000Z","updatedAt":"2020-04-23T20:00:09.000Z"}]},"administrative":{"replacements":[{"id":1,"administrativeId":1,"authorId":1,"absentee":"Jan Jacobs","substitute":"Geordy Hendricks","monitoring":true,"date":"2020-03-30T15:46:36.000Z","shift":true,"createdAt":"2020-04-23T20:00:09.000Z","updatedAt":"2020-04-23T20:00:09.000Z"}],"workplaceEvents":[{"id":1,"administrativeId":1,"authorId":1,"workplaceTypeId":1,"description":"Jacob sleutelbeen gebroken","absentee":"Jacob Franssen","substitute":"James Brook","monitoring":true,"date":"2020-12-11T09:10:23.000Z","shift":false,"createdAt":"2020-04-23T20:00:09.000Z","updatedAt":"2020-04-23T20:00:09.000Z"}],"secretariatNotifications":[{"id":1,"administrativeId":1,"authorId":1,"absentee":"Jan Jacobs","substitute":"Geordy Hendricks","monitoring":true,"date":"2020-03-30T15:46:36.000Z","shift":true,"createdAt":"2020-04-23T20:00:09.000Z","updatedAt":"2020-04-23T20:00:09.000Z"}]},"technical":{"defects":[{"id":1,"technicalId":1,"authorId":1,"defectTypeId":1,"description":"lekkende kraan in kamer 304","monitoring":true,"date":"2020-04-15T13:03:57.000Z","createdAt":"2020-04-23T20:00:09.000Z","updatedAt":"2020-04-23T20:00:09.000Z"},{"id":2,"technicalId":1,"authorId":1,"defectTypeId":2,"description":"krakende deur in kamer 512","monitoring":true,"date":"2020-04-15T16:58:34.000Z","createdAt":"2020-04-23T20:00:09.000Z","updatedAt":"2020-04-23T20:00:09.000Z"}],"malfunctions":[{"id":1,"technicalId":1,"authorId":1,"malfunctionTypeId":1,"description":"lekkende kraan in kamer 304","monitoring":true,"date":"2020-04-15T13:03:57.000Z","duration":6,"createdAt":"2020-04-23T20:00:09.000Z","updatedAt":"2020-04-23T20:00:09.000Z"}]}};
     },
 
     reportClick: function(id: string) {
