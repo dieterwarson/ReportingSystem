4fcb7109141f01a6083615568c6391f147bd91a9
Chass Beerts <chass.beerts@student.uhasselt.be>
2020-04-23 16:53:53 +0200

    added the checkboxes (commented) need to fix layout && made the active buttons different

--- ReportingSystem/reportingsystemVue/src/views/AddReport.vue
+++ ReportingSystem/reportingsystemVue/src/views/AddReport.vue
@@ -5,44 +5,175 @@
     <h1>Voeg verslag toe</h1>
             <form id="addReport">
                 <div class="btn-group d-flex" role="group" aria-label="Justified button group" >
-                    <button type="button" class="btn btn-primary" @click.prevent="getOperational">Operationeel</button>
-                    <button type="button" class="btn btn-primary" @click.prevent="getWorkforce">Personeel</button>
-                    <button type="button" class="btn btn-primary" @click.prevent="getTechnical">Technisch</button>
+                    <button id="operationalButton" type="button" class="btn btn-info" @click.prevent="getOperational">Operationeel</button>
+                    <button id="workForceButton" type="button" class="btn btn-primary" @click.prevent="getWorkforce">Personeel</button>
+                    <button id="technicalButton" type="button" class="btn btn-primary" @click.prevent="getTechnical">Technisch</button>
                 </div>
 
                 <section v-if="step == 'Operational'">
                     <h3>Operationeel</h3>
+                    <!-- <div class="row">
+                        <div class="checkbox-container text-sm-left col-lg-3" >
+                        <div class="text-sm-left">
+                            <div class="text-sm-left">
+                                <input class="text-sm-left" type="checkbox" name="cross-border pursuit" v-model="form.crossBorderPursuit"><label >Grensoverschrijdende achtervolging</label>
+                            </div>
+                            <div class="text-sm-left">
+                                <input class="text-sm-left" type="checkbox" name="Specific Event" v-model="form.specificEvent"><label >Specifieke Gebeurtenis</label>
+                            </div>
+                            <div class="text-sm-left">
+                                <input class="text-sm-left" type="checkbox" name="Proclamation BIN-alarm" v-model="form.BIN"><label >Afkondiging BIN-alarm</label>
+                            </div>
+                            <div class="text-sm-left">
+                                <input class="text-sm-left" type="checkbox" name="Telecom operator" v-model="form.telecomOperator"><label >Bevraging gsm-operatoren</label>
+                            </div>
+                            <div class="text-sm-left">
+                                <label class="text-sm-left">Signaleringen:</label>
+                                <div class="text-sm-left">
+                                    <div class="text-sm-left">
+                                        <input class="text-sm-left" type="checkbox" name="Signaling Person" v-model="form.signalingPerson"><label >Personen</label>
+                                    </div >
+                                    <div class="text-sm-left">
+                                        <input type="checkbox" name="Signaling Vehicle" v-model="form.signalingVehicle"><label >Voertuig</label>
+                                    </div>
+                                    <div class="text-sm-left">
+                                        <input class="text-sm-left" type="checkbox" name="Signaling Object" v-model="form.signalingObject"><label >Voorwerp</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="text-sm-left">
+                                <label class="text-sm-left">Verwittiging schouwarts:</label>
+                                <div class="text-sm-left">
+                                    <div class="text-sm-left">
+                                        <input class="text-sm-left" type="checkbox" name="Collocation" v-model="form.collocation"><label >Collocatie:</label>
+                                        <div class="text-sm-left">
+                                            <div class="text-sm-left">
+                                            <input class="text-sm-left" type="checkbox" name="First Schouwarts" v-model="form.firstSchouwarts"><label >Martijn Martens</label>
+                                            </div>
+                                            <div class="text-sm-left">
+                                                <input class="text-sm-left" type="checkbox" name="Second Schouwarts" v-model="form.secondSchouwarts"><label >Dirk Stegen</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="text-sm-left">
+                                        <input class="text-sm-left" type="checkbox" name="Set Sexual Agression" v-model="form.sexualAgression"><label >Sexuele agressie:</label>
+                                        <div class="text-sm-left">
+                                            <div class="text-sm-left">
+                                                <input class="text-sm-left" type="checkbox" name="First SA Schouwarts" v-model="form.firstSASchouwarts"><label >Ruben Boelen</label>
+                                            </div>
+                                            <div class="text-sm-left">
+                                                <input class="text-sm-left" type="checkbox" name="Second SA Schouwarts" v-model="form.secondSASchouwarts"><label >Vincent Potargent</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div> -->
+                    <!-- <div class="input-group col-lg-8"> -->
                     <div class="input-group">
+                        <div class="input-group"   style="height: 20%;">
                             <input name ="plNumber" v-model="form.plNumber" type="text" placeholder="PL-nummer" class="form-control form-control-lg">
-                            <button class="btn btn-large btn-block btn-success" type="button" @click="operationalReport">Zoek fiche</button>
-                        <input name="location" v-model="form.location" type="text" placeholder="Adres" class="form-control form-control-lg">
-                        <input name="date" v-model="form.operationalDate" type="text" placeholder="Datum" class="form-control form-control-lg">
+                        </div>
+                        <div class="input-group" style="height: 20%;">
+                            <button class="btn btn-block btn-success" type="button" @click.prevent="getFile">Zoek fiche</button>
+                        </div>
+                        <div class="input-group" style="height: 20%;" >
+                            <input name="location" v-model="form.location" type="text" placeholder="Adres" class="form-control form-control-lg">
+                            <input name="date" v-model="form.operationalDate" type="text" placeholder="Datum" class="form-control form-control-lg">
+                            <input name="unit" v-model="form.unit" type="text" placeholder="Unit" class="form-control form-control-lg">
+                        </div>  
+                        
+                        <div  class="input-group" style="height: 39%;"> 
+                            <input  v-model="form.operationalMessage" type="text" placeholder="Extra info" class="form-control form-control-lg">
+                        </div>
                     </div>
                     
-<!-- CODE FOR LOADING CHECKBOXES BY VALUES IN TS
-
-                    <checkbox-group>
-                        <checkbox-component v-for="(option, index) in operationalOptions" v-model="formData[option.label]" :key="option.id" :label="option.label" :name="name">
-                        </checkbox-component>
-                    </checkbox-group>
--->
-
-                    <input v-model="form.operationalMessage" type="text" placeholder="Data" class="form-control form-control-lg">
-                    <button class="btn btn-large btn-block btn-success" type="button" @click="operationalReport">Opslaan</button>
+                    <!-- </div> -->
+                    
+                    
+                    <button class="btn btn-large btn-block btn-success" type="button" @click.prevent="addReport">Opslaan</button>
+                    <small v-if="operationalFailed">Er is iets misgegaan bij het toevoegen aan de database</small>
+                    <small v-if="operationalSucceeded">Het verslag is toegevoegd</small>
 
                 </section>
 
 
                 <section v-if="step == 'Workforce'">
-                    <h3>Personeel</h3>
-                    <textarea v-model="form.workforceMessage" placeholder="Type your message" class="form-control form-control-lg"></textarea>
-                    <button class="btn btn-large btn-block btn-primary" type="button" @click.prevent="saveButton">Opslaan</button>
+                <h3>Personeel</h3>
+
+                    <!-- <div class="row">
+                        <div class="checkbox-container text-sm-left col-lg-3" >
+                        <div class="text-sm-left">
+                            <div class="text-sm-left">
+                                <input class="text-sm-left" type="checkbox" name="Signaling Person" v-model="form.signalingPerson"><label >Personeel</label>
+                                <div class="text-sm-left">
+                                    <div class="text-sm-left">
+                                        <input class="text-sm-left" type="checkbox" name="Signaling Person" v-model="form.signalingPerson"><label >Ziektemelding</label>
+                                    </div >
+                                    <div class="text-sm-left">
+                                        <input type="checkbox" name="Signaling Vehicle" v-model="form.signalingVehicle"><label >Arbeidsongeval</label>
+                                    </div>
+                                    <div class="text-sm-left">
+                                        <input class="text-sm-left" type="checkbox" name="Signaling Object" v-model="form.signalingObject"><label >Voorval tijdens dienst</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <input class="text-sm-left" type="checkbox" name="Signaling Person" v-model="form.signalingPerson"><label >Melding secretariaat</label>
+                            </div>
+                        </div> -->
+                    
+                    <!-- <div class="input-group col-lg-8"> -->
+                    <div class="input-group">
+                        <div class="input-group">
+                            <input name="Naam" v-model="form.absentee" type="text" placeholder="Naam medewerker" class="form-control form-control-lg">
+                            <input name ="Message" v-model="form.workforceDescription" type="text" placeholder="Extra info" class="form-control form-control-lg">
+                        </div>                    
+                    </div>
+                    <button class="btn btn-large btn-block btn-success" type="button" @click.prevent="addReport">Opslaan</button>
+                    <small v-if="operationalFailed">Er is iets misgegaan bij het toevoegen aan de database</small>
+                    <small v-if="operationalSucceeded">Het verslag is toegevoegd</small>
                 </section>
 
                 <section v-if="step == 'Technical'">
                     <h3>Technisch</h3>
-                    <textarea v-model="form.technicalMessage" placeholder="Type your message" class="form-control form-control-lg"></textarea>
-                    <button class="btn btn-large btn-block btn-primary" type="button" @click.prevent="saveButton">Opslaan</button>
+                    <div class="row">
+                        <div class="checkbox-container text-sm-left col-lg-3" >
+                        <div class="text-sm-left">
+                            <div class="text-sm-left">
+                                <input class="text-sm-left" type="checkbox" name="Signaling Person" v-model="form.signalingPerson"><label >Technisch WKS</label>
+                                <div class="text-sm-left">
+                                    <div class="text-sm-left">
+                                        <input class="text-sm-left" type="checkbox" name="Signaling Person" v-model="form.signalingPerson"><label >Voorwerp</label>
+                                    </div >
+                                    <div class="text-sm-left">
+                                        <input type="checkbox" name="Signaling Vehicle" v-model="form.signalingVehicle"><label >Verwittiging ASC</label>
+                                    </div>
+                                    <div class="text-sm-left">
+                                        <input class="text-sm-left" type="checkbox" name="Signaling Object" v-model="form.signalingObject"><label >Verwittiging anderen</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="text-sm-left">
+                                <input class="text-sm-left" type="checkbox" name="Signaling Person" v-model="form.signalingPerson"><label >Logistiek</label>
+                                <div class="text-sm-left">
+                                    <div class="text-sm-left">
+                                        <input class="text-sm-left" type="checkbox" name="Signaling Person" v-model="form.signalingPerson"><label >Defect</label>
+                                    </div >
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group col-lg-8">
+                    <div class="input-group">
+                        <div class="input-group">
+                            <input name ="Message" v-model="form.technicalMessage" type="text" placeholder="Extra info" class="form-control form-control-lg">
+                        </div>                    
+                    </div>
+                    <button class="btn btn-large btn-block btn-success" type="button" @click.prevent="addReport">Opslaan</button>
+                    <small v-if="operationalFailed">Er is iets misgegaan bij het toevoegen aan de database</small>
+                    <small v-if="operationalSucceeded">Het verslag is toegevoegd</small>
                 </section>     
 
             </form>         
@@ -64,7 +195,22 @@ export default Vue.extend({
             location: null,
             operationalDate: null,
             operationalMessage: null,
-            checkedOperationalBoxes: null,
+            crossBorderPursuit: null,
+            specificEvent: null,
+            BIN: null,
+            telecomOperator: null,
+            signalingPerson: null,
+            signalingVehicle: null,
+            signalingObject: null,
+            collocation: null,
+            firstSchouwarts: null,
+            secondSchouwarts: null,
+            sexualAgression: null,
+            firstSASchouwarts: null,
+            secondSASchouwarts: null,
+            operationalFailed: false,
+            operationalSucceeded: false,
+            unit: null,
 
             //WORKFORCE OBJECTS
             absent: null,
@@ -93,29 +239,72 @@ export default Vue.extend({
 
     methods: {
         getOperational : function() {
-            if (this.step != 'Operational')
-                this.step = 'Operational'
+            if (this.step != 'Operational'){
+                this.step = 'Operational';
+                document.getElementById("operationalButton").classList.replace('btn-primary', "btn-info");
+                document.getElementById("workForceButton").classList.replace("btn-info", 'btn-primary');
+                document.getElementById('technicalButton').classList.replace("btn-info", 'btn-primary');
+            }    
         },
         getWorkforce : function() {
-            if (this.step != 'Workforce')
+            if (this.step != 'Workforce'){
                 this.step = 'Workforce'
+                document.getElementById('workForceButton').classList.replace("btn-primary", "btn-info");
+                document.getElementById("operationalButton").classList.replace("btn-info", 'btn-primary');
+                document.getElementById('technicalButton').classList.replace("btn-info", 'btn-primary');
+            }
         },
         getTechnical : function() {
-            if (this.step != 'Technical')
+            if (this.step != 'Technical'){
                 this.step = 'Technical'
+                document.getElementById("operationalButton").classList.replace("btn-info", 'btn-primary');
+                document.getElementById('workForceButton').classList.replace("btn-info", 'btn-primary');
+                document.getElementById('technicalButton').classList.replace("btn-primary", "btn-info");
+            }
         },
 
         saveButton : function() {
             alert('send to db');
         },
-        async operationalReport() {
-            const response = await ReportingService.addReport({
+        async getFile() {
+            const response = await ReportingService.getFile({
                 plNumber: this.form.plNumber
-                })
-                this.form.plNumber = response.plNumber;
-                this.form.operationalDate = response.date.toString();
+                });
+                this.form.operationalDate = new Date(response.date).toLocaleString('en-BE').replace(",", "");
                 this.form.operationalMessage = response.actions;
                 this.form.location = response.location;
+                this.form.unit = response.unit;
+        },
+        async addReport() {
+            const response = await ReportingService.addReport({
+                plNumber: this.form.plNumber,
+                location: this.form.location,
+                date: this.form.operationalDate,
+                message: this.form.operationalMessage,
+                unit: this.form.unit
+            });
+            if (response.added) {
+            this.form.plNumber = null;
+            this.form.location = null;
+            this.form.operationalDate = null;
+            this.form.operationalMessage = null;
+            this.form.crossBorderPursuit = null;
+            this.form.specificEvent = null;
+            this.form.BIN = null;
+            this.form.telecomOperator = null;
+            this.form.signalingPerson = null;
+            this.form.signalingVehicle = null;
+            this.form.signalingObject = null;
+            this.form.collocation = null;
+            this.form.firstSchouwarts = null;
+            this.form.secondSchouwarts = null;
+            this.form.sexualAgression = null;
+            this.form.firstSASchouwarts = null;
+            this.form.secondSASchouwarts = null;
+            this.form.operationalSucceeded = true;
+            } else {
+                this.form.operationalFailed = true;
+            }
         }
     }
     
