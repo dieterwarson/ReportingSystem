c875db83cc228ab6ff3e4e40b178f1d7e4f445bd
MathiasJans <mathias_jans@student.uhasselt.be>
2020-05-14 09:40:18 +0200

    reportcontent in gezet

--- ReportingSystem/reportingsystemVue/src/views/ChangeEvent.vue
+++ ReportingSystem/reportingsystemVue/src/views/ChangeEvent.vue
@@ -243,66 +243,186 @@ export default Vue.extend({
           date: "2020-03-16T21:13:48.000Z",
           temporary: false,
           nightShift: true,
-          createdAt: "",
-          updatedAt: ""
+          createdAt: "2020-05-04T07:46:17.000Z",
+          updatedAt: "2020-05-04T07:46:17.000Z"
         },
         operational: {
-          operationalEvents: [{
-            id: 1,
-            authorId: 1,
-            operationalId: 1,
-            signaling: "Verlies inschrijvingsbewijs",
-            plNumber: "",
-            description: "",
-            monitoring: true,
-            location: "",
-            unit: "KEMPLA",
-            date: "2020-03-16T18:13:48.000Z",
-            createdAt: "2020-05-04T07:47:37.000Z",
-            updatedAt: "2020-05-04T07:47:37.000Z"
-          }, ]
+          operationalEvents: [
+            {
+              id: 1,
+              authorId: 1,
+              operationalId: 1,
+              signaling: "Verlies inschrijvingsbewijs",
+              plNumber: null,
+              description: null,
+              monitoring: true,
+              location: null,
+              unit: "KEMPLA",
+              date: "2020-03-16T18:13:48.000Z",
+              createdAt: "2020-05-04T07:47:37.000Z",
+              updatedAt: "2020-05-04T07:47:37.000Z"
+            },
+            {
+              id: 2,
+              authorId: 1,
+              operationalId: 1,
+              signaling: null,
+              plNumber: "PL03170104",
+              description: null,
+              monitoring: true,
+              location: null,
+              unit: "HANO",
+              date: "2020-03-16T22:05:18.000Z",
+              createdAt: "2020-05-04T07:47:37.000Z",
+              updatedAt: "2020-05-04T07:47:37.000Z"
+            },
+            {
+              id: 3,
+              authorId: 1,
+              operationalId: 1,
+              signaling: null,
+              plNumber: "PL031770168",
+              description: null,
+              monitoring: true,
+              location: null,
+              unit: "CARMA",
+              date: "2020-03-16T21:34:37.000Z",
+              createdAt: "2020-05-04T07:47:37.000Z",
+              updatedAt: "2020-05-04T07:47:37.000Z"
+            },
+            {
+              id: 4,
+              authorId: 1,
+              operationalId: 1,
+              signaling: "Seining persoon",
+              plNumber: null,
+              description: null,
+              monitoring: true,
+              location: null,
+              unit: "LAMA",
+              date: "2020-03-16T23:34:33.000Z",
+              createdAt: "2020-05-04T07:47:37.000Z",
+              updatedAt: "2020-05-04T07:47:37.000Z"
+            },
+            {
+              id: 5,
+              authorId: 1,
+              operationalId: 1,
+              signaling: "Seining persoon",
+              plNumber: null,
+              description: null,
+              monitoring: true,
+              location: null,
+              unit: "LOON",
+              date: "2020-03-16T23:57:10.000Z",
+              createdAt: "2020-05-04T07:47:37.000Z",
+              updatedAt: "2020-05-04T07:47:37.000Z"
+            },
+            {
+              id: 6,
+              authorId: 1,
+              operationalId: 1,
+              signaling: "Seining persoon",
+              plNumber: "PL03170202",
+              description: null,
+              monitoring: true,
+              location: null,
+              unit: "BIHORI",
+              date: "2020-03-16T00:18:57.000Z",
+              createdAt: "2020-05-04T07:47:37.000Z",
+              updatedAt: "2020-05-04T07:47:37.000Z"
+            },
+            {
+              id: 7,
+              authorId: 1,
+              operationalId: 1,
+              signaling: null,
+              plNumber: "PL03170104",
+              description: null,
+              monitoring: true,
+              location: null,
+              unit: "HANO",
+              date: "2020-03-16T00:45:45.000Z",
+              createdAt: "2020-05-04T07:47:37.000Z",
+              updatedAt: "2020-05-04T07:47:37.000Z"
+            },
+            {
+              id: 8,
+              authorId: 1,
+              operationalId: 1,
+              signaling: null,
+              plNumber: "PL03170315",
+              description: null,
+              monitoring: true,
+              location: null,
+              unit: "LRH",
+              date: "2020-03-16T01:21:25.000Z",
+              createdAt: "2020-05-04T07:47:37.000Z",
+              updatedAt: "2020-05-04T07:47:37.000Z"
+            },
+            {
+              id: 9,
+              authorId: 1,
+              operationalId: 1,
+              signaling: null,
+              plNumber: "PL03170322",
+              description: null,
+              monitoring: true,
+              location: null,
+              unit: "LRH",
+              date: "2020-03-16T01:51:47.000Z",
+              createdAt: "2020-05-04T07:47:37.000Z",
+              updatedAt: "2020-05-04T07:47:37.000Z"
+            }
+          ]
         },
         administrative: {
-          replacements: [{
-            id: 1,
-            authorId: 1,
-            administrativeId: 1,
-            absentee: "Jan Jacobs",
-            substitute: "Geordy Hendricks",
-            monitoring: true,
-            date: "2020-03-30T15:46:36.000Z",
-            shift: true,
-            createdAt: "2020-05-04T07:47:37.000Z",
-            updatedAt: "2020-05-04T07:47:37.000Z"
-          }],
+          replacements: [
+            {
+              id: 1,
+              authorId: 1,
+              administrativeId: 1,
+              absentee: "Jan Jacobs",
+              substitute: "Geordy Hendricks",
+              monitoring: true,
+              date: "2020-03-30T15:46:36.000Z",
+              shift: true,
+              createdAt: "2020-05-04T07:47:37.000Z",
+              updatedAt: "2020-05-04T07:47:37.000Z"
+            }
+          ],
           workplaceEvents: [],
-          secretariatNotifications: [{
-            id: 1,
-            authorId: 1,
-            administrativeId: 1,
-            absentee: "Jan Jacobs",
-            substitute: "Geordy Hendricks",
-            monitoring: true,
-            date: "2020-03-30T15:46:36.000Z",
-            shift: true,
-            createdAt: "2020-05-04T07:47:37.000Z",
-            updatedAt: "2020-05-04T07:47:37.000Z"
-          }]
+          secretariatNotifications: [
+            {
+              id: 1,
+              authorId: 1,
+              administrativeId: 1,
+              absentee: "Jan Jacobs",
+              substitute: "Geordy Hendricks",
+              monitoring: true,
+              date: "2020-03-30T15:46:36.000Z",
+              shift: true,
+              createdAt: "2020-05-04T07:47:37.000Z",
+              updatedAt: "2020-05-04T07:47:37.000Z"
+            }
+          ]
         },
         technical: {
           defects: [],
-          malfunctions: [{
-            id: 1,
-            authorId: 1,
-            technicalId: 1,
-            malfunctionTypeId: 1,
-            description: "lekkende kraan in kamer 304",
-            monitoring: true,
-            date: "2020-04-15T13:03:57.000Z",
-            duration: 6,
-            createdAt: "2020-05-04T07:47:37.000Z",
-            updatedAt: "2020-05-04T07:47:37.000Z"
-          }]
+          malfunctions: [
+            {
+              id: 1,
+              authorId: 1,
+              technicalId: 1,
+              malfunctionTypeId: 1,
+              description: "lekkende kraan in kamer 304",
+              monitoring: true,
+              date: "2020-04-15T13:03:57.000Z",
+              duration: 6,
+              createdAt: "2020-05-04T07:47:37.000Z",
+              updatedAt: "2020-05-04T07:47:37.000Z"
+            }
+          ]
         }
       },
       eventId: 0,
@@ -356,33 +476,22 @@ export default Vue.extend({
     this.operationalDescription = "edscrip";
   },
 
-  created() {
-    // this.form.operationalMessage = this.reportContent.operational.operationalEvents[
-    //   this.eventId - 1
-    // ].description;
-    // this.form.operationalMessage = "éhauohpuzeahafeu";
-    // this.operationalDescription = "edscrip";
-    // this.operationalDescription = this.reportContent.operational.operationalEvents[
-    //   this.eventId - 1
-    // ].description;
-    // this.form.operationalMessage = this.operationalDescription;
-  },
-
   methods: {
     setOperationalId: function (id: string) {
       this.operationalId = id;
     },
     loadData: function () {
-      ReportingService.getAllReports(
-        "/api/reports/content/" + this.$route.query.reportId
-      ).then(res => (this.reportContent = res));
+      // ReportingService.getAllReports(
+      //   "/api/reports/content/" + this.$route.query.reportId
+      // ).then(res => (this.reportContent = res));
 
-      ReportingService.getAllReports("/api/reports/types").then(
-        res => (this.reportTypes = res)
-      );
+      // ReportingService.getAllReports("/api/reports/types").then(
+      //   res => (this.reportTypes = res)
+      // );
 
       this.step = String(this.$route.query.categorie);
       this.eventId = parseInt(String(this.$route.query.eventId));
+
 
       // this.form.operationalMessage = this.reportContent.operational.operationalEvents[
       //   this.eventId - 1
