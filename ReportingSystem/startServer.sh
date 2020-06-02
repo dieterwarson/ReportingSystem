1e9a1fc98239880aa61630de788dc43681f15e6a
Dieter Warson <dieter.warson@student.uhasselt.be>
2020-06-02 22:22:06 +0200

    fix dockerfile

--- ReportingSystem/startServer.sh
+++ ReportingSystem/startServer.sh
@@ -1,8 +1,6 @@
 
 /etc/init.d/mysql start
 
-cd psopv-2020-groep1/ReportingSystem
-
 mysql -fu root -pmysqlroot < "./setup/mysql_secure_install.sql"
 mysql -fu root -pmysqlroot < "./setup/testdata.sql"
 
