f20d2316b0bf05fa791b314df5a7484cc52fd832
MathiasJans <mathias_jans@student.uhasselt.be>
2020-07-25 14:31:11 +0200

    Opmaak van multiselect voor zoekresultaat filters:
    vue-treeselect.d.ts:
    - toegevoegd om TypeScript mogelijk te maken
    
    Reports.ts:
    - autoformat
    - Treeselect component toegevoegd, declaratie en oproep
    - nodige data toegevoegd om velden te tonen

--- ReportingSystem/reportingsystemVue/src/views/Reports.vue
+++ ReportingSystem/reportingsystemVue/src/views/Reports.vue
@@ -4,62 +4,58 @@
     <router-link to="/">Startscherm</router-link>
   </div>
 
-<div v-if="reports" class="container">
-  
-  <h1>Verslagen</h1>
-  <div class="row">
-    <form class="col-md-4">
-      <p class="mb-1 text-sm-left">Selecteer hier de datums: </p>
-      <VueRangedatePicker
-              class="mb-5"
-              @selected="onDateSelected"
-              i18n="EN"
-              :months="months"
-              :shortDays="shortDays"
-              :captions="captions"
-              :presetRanges="presetRanges"
-            >
-      </VueRangedatePicker>
-    </form>
-    <div class="col-md-8">
-      <div class="container my-2" v-for="value in reports" :key="value.id">
-        <button class="btn btn-secondary btn-lg btn-block" v-on:click="reportClick(String(value.id))">
-          {{
-            new Date(value.date).toLocaleString("nl-BE", {
-              year: "numeric",
-              month: "numeric",
-              day: "numeric",
-              hour: "2-digit",
-              minute: "2-digit",
-              hour12: false
-            })
-          }}
-          <span class="badge badge-primary ml-3">{{ getShift(value.nightShift) }}</span>
-        </button>
+  <div v-if="reports" class="container">
+    <h1>Verslagen</h1>
+    <div class="row">
+      <form class="col-md-4">
+        <p class="mb-1 text-sm-left">Selecteer hier de datums:</p>
+        <VueRangedatePicker class="mb-5" @selected="onDateSelected" i18n="EN" :months="months" :shortDays="shortDays" :captions="captions" :presetRanges="presetRanges"></VueRangedatePicker>
+        <div class="filter-select">
+          <treeselect placeholder="Choose filters" v-model="value" :multiple="true" :options="options" />
+          <button type="button" class="btn btn-info">Filter</button>
+        </div>
+      </form>
+      <div class="col-md-8">
+        <div class="container my-2" v-for="value in reports" :key="value.id">
+          <button class="btn btn-secondary btn-lg btn-block" v-on:click="reportClick(String(value.id))">
+            {{
+              new Date(value.date).toLocaleString("nl-BE", {
+                year: "numeric",
+                month: "numeric",
+                day: "numeric",
+                hour: "2-digit",
+                minute: "2-digit",
+                hour12: false
+              })
+            }}
+            <span class="badge badge-primary ml-3">{{ getShift(value.nightShift) }}</span>
+          </button>
+        </div>
       </div>
     </div>
+    <vPagination :classes="bootstrapPaginationClasses" v-model="currentPage" :page-count="pages"></vPagination>
   </div>
-  <vPagination :classes="bootstrapPaginationClasses" v-model="currentPage" :page-count="pages"></vPagination>
-</div>
 </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import ReportingService from "../services/ReportingService";
-import vPagination from 'vue-plain-pagination';
+import vPagination from "vue-plain-pagination";
 import VueRangedatePicker from "vue-rangedate-picker";
+import Treeselect from "@riophae/vue-treeselect";
+import '@riophae/vue-treeselect/dist/vue-treeselect.css'
 
 interface DateRange {
   start: string;
   end: string;
 }
 
-
 export default Vue.extend({
   components: {
     vPagination,
-    VueRangedatePicker
+    VueRangedatePicker,
+    Treeselect
   },
   data: function () {
     return {
@@ -68,16 +64,16 @@ export default Vue.extend({
       interval: 0,
       currentPage: 1,
       bootstrapPaginationClasses: {
-        ul: 'pagination',
-        li: 'page-item',
-        liActive: 'active',
-        liDisable: 'disabled',
-        button: 'page-link'  
+        ul: "pagination",
+        li: "page-item",
+        liActive: "active",
+        liDisable: "disabled",
+        button: "page-link"
       },
       pages: 1,
       selectedDate: {
         start: "",
-        end: "",
+        end: ""
       },
       months: [
         "Januari",
@@ -91,15 +87,15 @@ export default Vue.extend({
         "September",
         "October",
         "November",
-        "December",
+        "December"
       ],
       shortDays: ["ma", "di", "woe", "do", "vrij", "zat", "zo"],
       captions: {
-        "title": "Kies de datums",
-        "ok_button": "Toepassen",
+        title: "Kies de datums",
+        okButton: "Toepassen"
       },
       presetRanges: {
-        today: function() {
+        today: function () {
           const n = new Date();
           const startToday = new Date(
             n.getFullYear(),
@@ -120,11 +116,11 @@ export default Vue.extend({
             active: false,
             dateRange: {
               start: startToday,
-              end: endToday,
-            },
+              end: endToday
+            }
           };
         },
-        thisMonth: function() {
+        thisMonth: function () {
           const n = new Date();
           const startMonth = new Date(
             Date.UTC(n.getFullYear(), n.getMonth(), 1)
@@ -137,14 +133,16 @@ export default Vue.extend({
             active: false,
             dateRange: {
               start: startMonth,
-              end: endMonth,
-            },
+              end: endMonth
+            }
           };
         },
         lastMonth: function () {
-          const n = new Date()
-          const startMonth = new Date(Date.UTC(n.getFullYear(), n.getMonth() - 1, 1))
-          const endMonth = new Date(Date.UTC(n.getFullYear(), n.getMonth(), 0))
+          const n = new Date();
+          const startMonth = new Date(
+            Date.UTC(n.getFullYear(), n.getMonth() - 1, 1)
+          );
+          const endMonth = new Date(Date.UTC(n.getFullYear(), n.getMonth(), 0));
           return {
             label: "Vorige maand",
             active: false,
@@ -152,12 +150,16 @@ export default Vue.extend({
               start: startMonth,
               end: endMonth
             }
-          }
+          };
         },
         last7days: function () {
-          const n = new Date()
-          const start = new Date(Date.UTC(n.getFullYear(), n.getMonth(), n.getDate() - 6))
-          const end = new Date(Date.UTC(n.getFullYear(), n.getMonth(), n.getDate()))
+          const n = new Date();
+          const start = new Date(
+            Date.UTC(n.getFullYear(), n.getMonth(), n.getDate() - 6)
+          );
+          const end = new Date(
+            Date.UTC(n.getFullYear(), n.getMonth(), n.getDate())
+          );
           return {
             label: "Vorige 7 dagen",
             active: false,
@@ -165,12 +167,16 @@ export default Vue.extend({
               start: start,
               end: end
             }
-          }
+          };
         },
         last30days: function () {
-          const n = new Date()
-          const start = new Date(Date.UTC(n.getFullYear(), n.getMonth(), n.getDate() - 30))
-          const end = new Date(Date.UTC(n.getFullYear(), n.getMonth(), n.getDate()))
+          const n = new Date();
+          const start = new Date(
+            Date.UTC(n.getFullYear(), n.getMonth(), n.getDate() - 30)
+          );
+          const end = new Date(
+            Date.UTC(n.getFullYear(), n.getMonth(), n.getDate())
+          );
           return {
             label: "Vorige 30 dagen",
             active: false,
@@ -178,9 +184,141 @@ export default Vue.extend({
               start: start,
               end: end
             }
-          } 
+          };
         }
       },
+      // define the default value
+      value: [],
+      options: [{
+          id: 'signaling',
+          label: 'Seining',
+          children: [{ // names of all signalings
+            id: 'signaling-jan',
+            label: 'Seining Jan',
+          }],
+        }, {
+          id: 'plNumber',
+          label: 'PL-nummer',
+          children: [{ // names of all plNumbers
+            id: 'plNumber',
+            label: '007',
+          }],
+        }, {
+          id: 'description',
+          label: 'Beschrijving',
+          children: [{ // names of all descriptions
+            id: 'description',
+            label: 'bes',
+          }],
+        }, {
+          id: 'location',
+          label: 'Adres',
+          children: [{ // names of all locations
+            id: 'location-hasselt',
+            label: 'Hasselt',
+          }],
+        }, {
+          id: 'unit',
+          label: 'Unit',
+          children: [{ // names of all units
+            id: 'unit-kampla',
+            label: 'KAMPLA',
+          }],
+        }, {
+          id: 'absentee',
+          label: 'Afwezige',
+          children: [{ // names of all personel
+            id: 'absentee-jan',
+            label: 'Jan',
+          }],
+        },
+        {
+          id: 'substitute',
+          label: 'Vervanger',
+          children: [{ // names of all personel
+            id: 'substitute-jan',
+            label: 'Jan',
+          }],
+        },
+        {
+          id: 'types',
+          label: 'Types',
+          children: [ // all types
+            {
+              id: 'operational-types',
+              label: 'Operationeel',
+              children: [ // all operational types
+                {
+                  id: 'operational-types-operator',
+                  label: 'Bevraging GSM operatoren',
+                },
+                {
+                  id: 'operational-types-bin',
+                  label: 'BIN-alarm',
+                },
+                {
+                  id: 'operational-types-pursuit',
+                  label: 'Grensoverschrijdende achtervolging',
+                },
+                {
+                  id: 'operational-types-signaling',
+                  label: 'Signalering',
+                },
+                {
+                  id: 'operational-types-specific',
+                  label: 'Specifieke gebeurtenis',
+                },
+                {
+                  id: 'operational-types-helicopter',
+                  label: 'Zoeken met helikopter',
+                },
+              ],
+            },
+            {
+              id: 'workplaceevent-types',
+              label: 'Voorval tijdens de dienst',
+              children: [ // all workplaceevent types
+                {
+                  id: 'workplaceevent-types-accident',
+                  label: 'Arbeidsongeval',
+                },
+                {
+                  id: 'workplaceevent-types-sick',
+                  label: 'Ziekte',
+                },
+              ],
+            },
+            {
+              id: 'administrative-types',
+              label: 'Logistiek',
+              children: [ // all administrative types
+                {
+                  id: 'administrative-types-vehicle',
+                  label: 'Schade aan voertuig',
+                },
+              ],
+            },
+            {
+              id: 'technical-types',
+              label: 'Technisch',
+              children: [ // all technical types
+                {
+                  id: 'technical-types-announcement-others',
+                  label: 'Verwittiging (anderen)',
+                },
+                {
+                  id: 'technical-types-announcement-asc',
+                  label: 'Verwittiging ASC',
+                },
+                {
+                  id: 'technical-types-object',
+                  label: 'Voorwerp',
+                },
+              ],
+            }
+          ],
+        }
+      ],
     };
   },
   created() {
@@ -194,22 +332,23 @@ export default Vue.extend({
 
   methods: {
     loadData: function () {
-      ReportingService.getPaginationReports(this.currentPage*10 - 10, this.selectedDate).then(
-        res => (this.reports = res)
-      );
+      ReportingService.getPaginationReports(
+        this.currentPage * 10 - 10,
+        this.selectedDate
+      ).then(res => (this.reports = res));
     },
-    onDateSelected: function(daterange: DateRange) {
+    onDateSelected: function (daterange: DateRange) {
       this.selectedDate = daterange;
     },
 
-    loadCount: function(){
-      ReportingService.getReportCount(this.selectedDate).then(
-        res => (this.calculatePages(res.count))
+    loadCount: function () {
+      ReportingService.getReportCount(this.selectedDate).then(res =>
+        this.calculatePages(res.count)
       );
     },
 
-    calculatePages: function(count: number) {
-      this.pages = Math.ceil(count/10);
+    calculatePages: function (count: number) {
+      this.pages = Math.ceil(count / 10);
     },
 
     reportClick: function (id: string) {
@@ -222,16 +361,13 @@ export default Vue.extend({
     },
 
     getShift: function (nightShift: boolean) {
-      if (nightShift)
-        return "Nachtshift ☾";
+      if (nightShift) return "Nachtshift ☾";
 
       return "Dagshift 🌣";
-    },
-
-    
+    }
   },
-  beforeDestroy: function() {
-      window.clearInterval(this.interval);
+  beforeDestroy: function () {
+    window.clearInterval(this.interval);
   },
   watch: {
     currentPage: {
@@ -240,7 +376,7 @@ export default Vue.extend({
         this.loadData();
         // this.loaded = true;
       },
-      deep: true,
+      deep: true
     },
     selectedDate: {
       handler() {
@@ -248,14 +384,14 @@ export default Vue.extend({
         this.loadData();
         // this.loaded = true;
       },
-      deep: true,
+      deep: true
     },
 
     reports: {
       handler() {
         this.loadCount();
       },
-      deep: true,
+      deep: true
     }
   }
 });
