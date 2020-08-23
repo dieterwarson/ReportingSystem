e424b1b34204c9ad4867459f23ea9bc48639ad6e
Dieter Warson <dieter.warson@student.uhasselt.be>
2020-08-23 21:30:07 +0200

    Revert "Non dev docker start script fix"
    This reverts commit 32db59ab4e9d44744701ee8f8598f98b371767f4.

--- ReportingSystem/startServer.sh
+++ ReportingSystem/startServer.sh
@@ -5,10 +5,6 @@ mysql -fu root -pmysqlroot < "./setup/testdata.sql"
 mysql -fu root -pmysqlroot < "./setup/levenshtein.sql"
 mysql -fu root -pmysqlroot < "./setup/indexes.sql"
 
-cd reportingsystemVue
-npm install 
-yarn build --dest ../reportingsystemExpress/src/public 
-cd -
 cd reportingsystemExpress
 
 npm run start:dev
